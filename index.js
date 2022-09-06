d17fd132469cf31c721828c791dd72684c51b596
michaels90up <michael1990spw@gmail.com>
2022-09-06 16:46:58 +0200

    Update chalk

---
+++ index.js
@@ -0,0 +1,7 @@
+import { argv } from 'node:process';
+import chalk from 'chalk';
+import color from 'randomcolor';
+import randomColor from 'randomcolor';
+
+console.log(argv);
+console.log(randomColor());
