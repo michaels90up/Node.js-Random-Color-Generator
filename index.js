7eebe323a222e2661be85d27503b5e2e9518489f
michaels90up <michael1990spw@gmail.com>
2022-09-07 15:54:18 +0200

    Update libraries

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import { argv } from 'node:process';
-import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
 const color = randomColor();
 console.log(color);
-// console.log(chalk.hex(color);
-const userTry =
+
+console.log('###############################');
+console.log('###############################');
+console.log('###############################');
+console.log('####                       ####');
+console.log('####                       ####');
+console.log('####                       ####');
+console.log('###############################');
+console.log('###############################');
+console.log('###############################');
