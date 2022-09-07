0e81171258da9bf4c542d6fe2329d4c7d839b318
michaels90up <michael1990spw@gmail.com>
2022-09-07 17:00:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import { argv } from 'node:process';
+import kuler from 'kuler';
 import randomColor from 'randomcolor';
 
-const color = randomColor();
-console.log(color);
+// from kuler library: const str = kuler('foo', color);
 
-console.log('###############################');
-console.log('###############################');
-console.log('###############################');
-console.log('####                       ####');
-console.log('####                       ####');
-console.log('####                       ####');
-console.log('###############################');
-console.log('###############################');
-console.log('###############################');
+const color = randomColor({
+  luminosity: process.argv[3],
+  hue: process.argv[2],
+});
+
+console.log(kuler('###############################', color));
+console.log(kuler('###############################', color));
+console.log(kuler('###############################', color));
+console.log(kuler('####                       ####', color));
+console.log(kuler(`####        ${color}        ####`, color));
+console.log(kuler('####                       ####', color));
+console.log(kuler('###############################', color));
+console.log(kuler('###############################', color));
+console.log(kuler('###############################', color));
